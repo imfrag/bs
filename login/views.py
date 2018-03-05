2b0ef9c17d2fe68688d566595cc1996834cd4722
imfrag <963677394@qq.com>
2018-03-05 23:42:39 +0800

    modify login application views, handle GET request

--- login/views.py
+++ login/views.py
@@ -8,21 +8,24 @@ from django.views.decorators.http import require_POST
 def signin(request, level=1):
     if level not in [1, 2, 3]:
         # 返回 404.html
-        pass
+        return render(request, '404.html')
 
     if request.method == 'GET':
         # 处理get方法请求，返回登录界面
+
         if level == 1:
             # 返回用户登录页面 login_user.html
-            pass
+            return render(request, 'login/user_signin.html')
         elif level == 2:
-            # 返回物业管理页面 login_mange.html
-            pass
+            # 返回物业管理页面 login_manage.html
+            return render(request, 'login/manage_signin.html')
         elif level == 3:
             # 返回系统管理页面 login_admin.html
-            pass
+            return render(request, 'login/admin_signin.html')
+
     elif request.method == 'POST':
         # 处理post方法请求，处理客户端请求数据，并验证
+
         if level == 1:
             # 验证用户登录
             pass
@@ -32,6 +35,7 @@ def signin(request, level=1):
         elif level == 3:
             # 验证系统管理员登录
             pass
+
     else:
         pass
 
