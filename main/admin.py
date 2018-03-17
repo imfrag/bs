71e4c8842fa2b99545c032c02caba339c79e4b9a
imfrag <963677394@qq.com>
2018-03-17 18:53:18 +0800

    register entity and relation in admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(HouseHolder)
+admin.site.register(House)
+admin.site.register(Car)
+admin.site.register(Repair)
+admin.site.register(Staff)
+admin.site.register(Profile)
+admin.site.register(Billboard)
+admin.site.register(RelHouseHolder)
+admin.site.register(RelCarHolder)
+admin.site.register(RelHouseholderRepair)
+admin.site.register(RelStaffProfile)
